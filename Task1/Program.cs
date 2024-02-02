e19d7bf22c09647f33d3081fbe4d59ad91a7c797
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-02-02 22:49:19 +0500

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿void writeNumbersInTheInterval(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine(m);
+        writeNumbersInTheInterval(m + 1, n);
+    }
+}
+
+int m = 3;
+int n = 12;
+writeNumbersInTheInterval(m, n);
