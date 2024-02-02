e29844151eb64c96ea26eda17091d4f61b1f07f2
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-02-02 23:23:50 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int AckermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n = n + 1;
+    }
+    else if (m > 0 & n == 0)
+    {
+        return AckermanFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    }
+}
+int m = 2;
+int n = 4;
+int result = AckermanFunction(m, n);
+Console.WriteLine(result);
