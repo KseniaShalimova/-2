0f41da2a05c321596d5a76ebb6a6ee6127aee9bd
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-02-03 00:07:45 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = [1, 2, 5, 0, 10, 34, 90];
+int i = 0;
+void reversArray(int[] arr, int i)
+{
+    if (i < arr.Length)
+    {
+        Console.WriteLine(arr[arr.Length - 1 - i]);
+        reversArray(arr, i + 1);
+    }
+}
+
+reversArray(arr, i);
